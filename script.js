ab7511fc8664c8ec4151d37347d2dcb037ccde54
renatogsouza <renatomacedo1995@gmail.com>
2021-03-20 22:50:16 -0300

    adicionando palavras a um array

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ const input = document.getElementById('carta-texto');
 const btnGerar = document.getElementById('criar-carta');
 const paragrafo = document.getElementById('carta-gerada');
 let classesCarta = ['skewright', 'skewleft', 'rotateright', 'rotateleft', 'reallybig', 'big', 'medium', 'magazine2', 'magazine1', 'newspaper'];
-let classAleatorio = Math.floor(Math.random()*classesCarta.length - 1)
+
 function criarCarta() {
   btnGerar.addEventListener('click', function(event){
+    let frase = input.value;
+    let arrInput = frase.split(' ');
     verificaInput();
-    paragrafo.appendChild(novoSpan);
-    novoSpan.innerHTML = input.value;
-    novoSpan.classList.add(classesCarta[classAleatorio]);
+    for (let i = 0; i < arrInput.length; i++) {
+      let j = 0;
+      let classAleatorio = Math.floor(Math.random()*classesCarta.length - 1);
+      paragrafo.appendChild(novoSpan);
+      novoSpan.innerHTML = arrInput[i];
+      novoSpan.classList.add(classesCarta[classAleatorio]);
+      j++;
+    }
   });
 }
 criarCarta();
