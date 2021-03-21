ee94411b72d496fadc472ff10fbe2c17ff8505e8
Flavio Andrade <foa12381@gmail.com>
2021-03-20 22:47:42 -0300

    adiciona alert

---
+++ script.js
@@ -0,0 +1,24 @@
+const inputTextCart = document.querySelector('#carta-texto');
+const textoMisterioso = document.querySelector('#carta-gerada');
+const btnGeraCarta = document.querySelector('#criar-carta');
+const contadorLetras = document.querySelector('#carta-contador');
+
+function getInput() {
+  let geraTexto = inputTextCart.value;
+  btnGeraCarta.addEventListener('click', () => {
+    checkVazio();
+    console.log(geraTexto);
+  });
+}
+
+function checkVazio() {
+  if (inputTextCart.value === '' || inputTextCart === ' ' || inputTextCart === null) {
+    window.alert('Por favor, digite o conteúdo da carta.');
+  }
+}
+
+window.onload = () => {
+  getInput();
+
+
+};
