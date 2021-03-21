b73ae4c465ced9814088d4b8684510ee9a66349e
Marcos Mantovani <mvmes23@gmail.com>
2021-03-21 13:37:43 -0300

    added new feature Counter! to count number of words

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const paiInputText = document.querySelector('#carta-gerada');
 const criarCartaBtn = document.querySelector('#criar-carta');
 const textInput = document.querySelector('#carta-texto');
+const textCounter = document.querySelector('#carta-contador');
 let whitecount = 0;
 
+
 const criarFrase = () => {
-  let text = textInput.value;
-  text = text.split(' ');
+ let text = textInput.value;
+ text = text.split(' ');
   for (let index = 0; index < text.length; index += 1) {
     const createSpan = document.createElement('span');
     createSpan.innerHTML = text[index];
@@ -39,6 +41,16 @@ const createBtnClick = () => {
   });
 };
 
+const textCounterFunction = () => {   
+  textInput.addEventListener('keyup', () => {
+    let text = textInput.value;
+    text = text.split(' ');      
+    const maxCount = text.length;
+    textCounter.innerHTML = `${maxCount}`;
+  })    
+  }
+
 window.onload = () => {
   createBtnClick();
+  textCounterFunction();
 };
