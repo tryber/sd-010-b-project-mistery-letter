2eec89bcd1753b4e48fb3107fb71fb2a635b0bc7
renatogsouza <renatomacedo1995@gmail.com>
2021-03-20 23:24:53 -0300

    ajustando detalhes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const input = document.getElementById('carta-texto');
 const btnGerar = document.getElementById('criar-carta');
 const paragrafo = document.getElementById('carta-gerada');
 let classesCarta = ['skewright', 'skewleft', 'rotateright', 'rotateleft', 'reallybig', 'big', 'medium', 'magazine2', 'magazine1', 'newspaper'];
-
+let classAleatorio = Math.floor(Math.random()*classesCarta.length - 1);
 function criarCarta() {
   btnGerar.addEventListener('click', function(event){
     let frase = input.value;
@@ -14,7 +14,7 @@ function criarCarta() {
       let novoSpan = document.createElement('span');
       let classAleatorio = Math.floor(Math.random()*classesCarta.length - 1);
       paragrafo.appendChild(novoSpan);
-      novoSpan.innerHTML = arrInput[i];
+      novoSpan.innerHTML = arrInput[i] + ' ';
       novoSpan.classList.add(classesCarta[classAleatorio]);
     }
   });
