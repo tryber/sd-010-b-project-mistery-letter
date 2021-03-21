79c410dd10539bceac5d1cba9d11f105bc5f326a
renatogsouza <renatomacedo1995@gmail.com>
2021-03-21 00:07:03 -0300

    mudando a classe da span ao click

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-
 const para = document.getElementById('carta-gerada');
 const input = document.getElementById('carta-texto');
 const btnGerar = document.getElementById('criar-carta');
 const paragrafo = document.getElementById('carta-gerada');
 let classesCarta = ['skewright', 'skewleft', 'rotateright', 'rotateleft', 'reallybig', 'big', 'medium', 'magazine2', 'magazine1', 'newspaper'];
-let classAleatorio = Math.floor(Math.random()*classesCarta.length - 1);
+let classAleatorio = Math.floor(Math.random()*classesCarta.length);
+let spanClass;
+
 function criarCarta() {
-  btnGerar.addEventListener('click', function(event){
+  btnGerar.addEventListener('click', function(){
     let frase = input.value;
     let arrInput = frase.split(' ');
     verificaInput();
     for (let i = 0; i < arrInput.length; i++) {
       let novoSpan = document.createElement('span');
-      let classAleatorio = Math.floor(Math.random()*classesCarta.length - 1);
+      let classAleatorio = Math.floor(Math.random()*classesCarta.length);
       paragrafo.appendChild(novoSpan);
       novoSpan.innerHTML = arrInput[i] + ' ';
       novoSpan.classList.add(classesCarta[classAleatorio]);
-    }
+    }  
+    mudarClass();
   });
 }
 criarCarta();
@@ -25,4 +27,15 @@ function verificaInput() {
   if (input.value == '') {
     para.innerHTML ='Por favor, digite o conteúdo da carta';
   }
+}
+function mudarClass() {
+  spanClass = document.getElementsByTagName('span');
+  for (let i = 0; i < spanClass.length; i++) {
+  spanClass[i].addEventListener('click', function(event) {
+    let classAleatorio = Math.floor(Math.random()*classesCarta.length);
+    event.target.removeAttribute('class');
+    event.target.classList.add(classesCarta[classAleatorio]);
+    console.log('clicou');
+  });
+}
 }
