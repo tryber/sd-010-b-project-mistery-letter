69077757bfcb9720c416851a6f35c930057a2fac
renatogsouza <renatomacedo1995@gmail.com>
2021-03-20 21:39:12 -0300

    adicionando classe aleatoria no span

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const newSpan = document.createElement('span');
+let novoSpan = document.createElement('span');
 const para = document.getElementById('carta-gerada');
 const input = document.getElementById('carta-texto');
 const btnGerar = document.getElementById('criar-carta');
 const paragrafo = document.getElementById('carta-gerada');
-
+let classesCarta = ['skewright', 'skewleft', 'rotateright', 'rotateleft', 'reallybig', 'big', 'medium', 'magazine2', 'magazine1', 'newspaper'];
+let classAleatorio = Math.floor(Math.random()*classesCarta.length - 1)
 function criarCarta() {
   btnGerar.addEventListener('click', function(event){
     verificaInput();
-    paragrafo.appendChild(newSpan);
-    newSpan.innerHTML = input.value;
+    paragrafo.appendChild(novoSpan);
+    novoSpan.innerHTML = input.value;
+    novoSpan.classList.add(classesCarta[classAleatorio]);
   });
 }
 criarCarta();
@@ -18,5 +20,3 @@ function verificaInput() {
     para.innerHTML ='Por favor, digite o conteúdo da carta';
   }
 }
-
-    
