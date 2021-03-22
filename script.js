e9d59f421a06ef8abe73db8cd803ec485e974fb2
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-22 10:19:11 -0300

    add exercicios 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 e 16

--- script.js
+++ script.js
@@ -2,17 +2,35 @@ const newLetterButton = document.getElementById('criar-carta');
 const inputLetter = document.getElementById('carta-texto');
 const outputLetter = document.getElementById('carta-gerada');
 
+function generatesRandomClasses() {
+  const classes = [
+    'newspaper',
+    'magazine1',
+    'magazine2',
+    'medium',
+    'big',
+    'reallybig',
+    'rotateleft',
+    'rotateright',
+    'skewleft',
+    'skewright',
+  ];
+  const randomClasses = Math.floor(Math.random() * classes.length);
+  return classes[randomClasses];
+}
+
 function generateLetter() {
   newLetterButton.addEventListener('click', () => {
-    if (inputLetter.value === '') {
-      outputLetter.innerHTML = '';
-      outputLetter.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    outputLetter.innerHTML = '';
+    if (inputLetter.value === '' || inputLetter.value === ' ' || inputLetter.value === null) {
+      outputLetter.innerText = 'Por favor, digite o conteúdo da carta.';
     } else {
       outputLetter.innerHTML = '';
       const arrayString = inputLetter.value.split(' ');
       for (let index = 0; index < arrayString.length; index += 1) {
         const spanWord = document.createElement('span');
         spanWord.innerHTML = `${arrayString[index]}`;
+        spanWord.classList.add(generatesRandomClasses());
         outputLetter.appendChild(spanWord);
       }
     }
