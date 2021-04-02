802e3962bf9ce99eaa3bd9bbf7dc5a3ba46103c5
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-04-02 12:24:53 -0300

    requisito 3

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const botaoCriarCarta = document.getElementById('criar-carta');
 let inputRecebeCarta = document.getElementById('carta-texto');
 const paragrafoCarta = document.getElementById('carta-gerada');
+
 botaoCriarCarta.addEventListener('click', () => {
-  const conteudoInput = inputRecebeCarta.value;
-  for (let i = 0; i < conteudoInput.length; i += 1) {
-    console.log(conteudoInput[i]);
+  let conteudo = inputRecebeCarta.value;
+  let conteudoSeparado = conteudo.split(' ');
+  for (let i = 0; i < conteudoSeparado.length; i += 1) {
+    let span = document.createElement('span');
+    span.innerText = `${conteudoSeparado[i]} `;
+    paragrafoCarta.appendChild(span);
   }
 });
